6c8cb936341916d1de91e90f7dbbec1ac74dfc07
Kevin Marin <kevinmarinsaavedra@gmail.com>
2024-03-13 15:35:30 +0100

    Initial Commit

---
+++ BasicCameraManagement/BasicCameraManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicCameraManagementApp.swift
+//  BasicCameraManagement
+//
+//  Created by Kevin Marin on 13/3/24.
+//
+
+import SwiftUI
+
+@main
+struct BasicCameraManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
